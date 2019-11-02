2c33907cde776940ae4782914bb4d565f8dbaab4
reeslabree-student <42980135+reeslabree-student@users.noreply.github.com>
2019-11-01 20:56:41 -0600

    added homework2

---
+++ homework2/theory/coding.js
@@ -0,0 +1,37 @@
+function display2_1() {
+  var myanswer  = "1. undefined 2. true 3. false | The reason that one and two are different is because null and undefined are equal in value but different in datatype. == does not take into account data type and only compares value. === compares value and dataype." // TODO 2.1: Fill in Answer here after the :
+  document.getElementById("21answer").innerHTML = myanswer; // Do not change
+}
+function display2_2() {
+  var myanswer  = "This script would not run with the 'use strict' line. This specifies that you cannot run using undeclared variables, etc. Without the 'use strict' line this script will run, but is just a less secure javascript with a syntax error." // TODO 2.2: Fill in Answer here
+  document.getElementById("22answer").innerHTML = myanswer; // Do not change
+}
+
+function display2_5() {
+  var myanswer  = "These elements exist on the page, but their style.display is set to 'none', which triggers them not to be shown on the page. Regardless, they are still part of the document and consequently still part of the DOM" // TODO 2.5: Fill in Answer here
+  document.getElementById("25answer").innerHTML = myanswer; // Do not change
+}
+
+function countDiv() { // Do not add or remove lines to this function
+    var divs = document.getElementsByTagName("div"); // TODO: Question 2.3: Use the document Object to get the number of divs in the HTML page. Replace dummyMethod with the correct one.
+    alert("Number of divs in this page is: " + divs.length); // Do not change 
+}
+
+function makeOrange() { // Do not add or remove lines to this function
+    var narutos = document.getElementsByName("naruto"); // TODO: Question 2.4: Use the document Object to get all tags named "naruto" in the HTML page. Replace dummyMethod with the correct one.
+    for(var i=0; i< narutos.length; i++) { // Do not change
+        narutos[i].style.color = 'orange'; // Do not change
+    }
+}
+
+function getChildrenTags() { // Do not add or remove lines to this function
+  var children = document.getElementById("div_1134").children; // TODO: Question 2.5 Use the appropriate function to get all the children of a div named 'div_1134'
+  var tagNames = ""; // Do not change
+  for (var i = 0; i < children.length; i++) {// Do not change
+    console.log(children[i].tagName);// Do not change
+    tagNames += children[i].tagName +", " ;// Do not change
+  }// Do not change
+  alert(children.length); // Do not change
+  alert("The tags that I found are: " + tagNames);// Do not change
+}
+
